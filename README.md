19094be01aec818e22e68c0973d6e8355b6263b2
egvo <work.egvo@ya.ru>
2021-01-15 00:34:30 +0400

    Prepared package.

---
+++ README.md
@@ -0,0 +1,6 @@
+# google-sheets-db
+Google Sheets as a database ORM
+
+### 1.0.0 (2021-01-14)
+
+- Init version.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="google-sheets-db",
+    version="1.0.0",
+    description="Google Sheets as a database ORM",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egorgvo/google-sheets-db",
+    author="Egor Gvo",
+    author_email="work.egvo@ya.ru",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["google_sheets_db"],
+    include_package_data=True,
+)
