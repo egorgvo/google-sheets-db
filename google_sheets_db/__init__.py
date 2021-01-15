b51acc1155dd78081f241cc96d94f145e763d99b
egvo <work.egvo@ya.ru>
2021-01-15 15:19:51 +0400

    Version 1.0.4.

--- google_sheets_db/__init__.py
+++ google_sheets_db/__init__.py
@@ -1,7 +1,7 @@
-from field import Field
-from database import GoogleSheetsDB
-from base_sheet import BaseSheet
+from google_sheets_db.field import Field
+from google_sheets_db.database import GoogleSheetsDB
+from google_sheets_db.base_sheet import BaseSheet
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 __all__ = ["GoogleSheetsDB", "BaseSheet", "Field"]
